152a04951d008ebf67993d8642f1929460e886e1
yashashree7 <nirmaleyashashree@gmail.com>
2025-03-01 14:54:55 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,6 +5,8 @@ st.title('🤖Machine learning app')
 
 st.info('This is a app that builds a machine learning model!')
 
+with st.expander('Data'):
+  st.write('**Parameters**')
 df = pd.read_csv('https://raw.githubusercontent.com/yashashree7/predictive-maintanence/refs/heads/master/streamlit_app.py')
 df
 
